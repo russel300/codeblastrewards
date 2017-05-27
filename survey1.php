013b141b5ba18db4a3776324794423cfd49d707f
Russel Calvin <rusel300@yahoo.com>
2017-05-27 10:19:28 +0300

    The user profile page

---
+++ survey1.php
@@ -0,0 +1,192 @@
+<?php include "includes/header.php" ?>
+<body>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-md-offset-4">
+				<div class="login-panel panel panel-default">
+					<div class="panel-heading">
+						<h3 class="panel-title">Welcome User</h3>
+					</div>
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-lg-3 col-md-6">
+								<div class="panel panel-primary">
+									<div class="panel-heading">
+										<div class="row">
+											<div class="col-xs-3">
+												<i class="fa fa-comments fa-5x"></i>
+											</div>
+											<div class="col-xs-9 text-right">
+												<div class="huge">26</div>
+												<div>POINTS</div>
+											</div>
+										</div>
+									</div>
+									<a href="#">
+										<div class="panel-footer">
+											<span class="pull-left">View Points</span>
+											<span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+											<div class="clearfix"></div>
+										</div>
+									</a>
+								</div>
+							</div>
+							<div class="col-lg-3 col-md-6">
+								<div class="panel panel-green">
+									<div class="panel-heading">
+										<div class="row">
+											<div class="col-xs-3">
+												<i class="fa fa-certificate fa-5x"></i>
+											</div>
+											<div class="col-xs-9 text-right">
+												<div class="huge">2</div>
+												<div>New SURVEYS!</div>
+											</div>
+										</div>
+									</div>
+									<a href="#">
+										<div class="panel-footer">
+											<span class="pull-left">View Profile</span>
+											<span class="pull-right"><i class="fa fa-user"></i></span>
+											<div class="clearfix"></div>
+										</div>
+									</a>
+								</div>
+							</div>
+							<div class="col-lg-3 col-md-6">
+								<div class="panel panel-yellow">
+									<div class="panel-heading">
+										<div class="row">
+											<div class="col-xs-3">
+												<i class="fa fa-wrench fa-5x"></i>
+											</div>
+											<div class="col-xs-9 text-right">
+											</div>
+										</div>
+									</div>
+									<a href="#">
+										<div class="panel-footer">
+											<span class="pull-left">Edit Profile</span>
+											<span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+											<div class="clearfix"></div>
+										</div>
+									</a>
+								</div>
+							</div>
+							<div class="col-lg-3 col-md-6">
+								<div class="panel panel-red">
+									<div class="panel-heading">
+										<div class="row">
+											<div class="col-xs-3">
+												<i class="fa fa-user fa-5x"></i>
+											</div>
+											<div class="col-xs-9 text-right">
+											</div>
+										</div>
+									</div>
+									<a href="#">
+										<div class="panel-footer">
+											<span class="pull-left">Logout</span>
+											<span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+											<div class="clearfix"></div>
+										</div>
+									</a>
+								</div>
+							</div>
+						</div>
+						<!-- /.row -->
+						<!-- /.panel-heading -->
+						<!-- /.panel-heading -->
+
+						<!-- .panel-body -->
+					</div>
+
+					<!-- /.panel -->
+				</div>
+			</div>
+		</div>
+
+
+		<!-- /.row -->
+		<div class="row">
+			<div class="col-md-6 col-md-offset-4">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						Basic Form Elements
+					</div>
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-lg-3">
+								<form role="form">
+									<div class="form-group">
+										<p class="question-title" style="font-size: 12px; color: #424242; font-weight: bold; margin-top: 50px;">Considering only your most recent purchase experience, how likely would you be to recommend the purchase of an Apple iPad to a friend or colleague? (0 is not at all likely, 10 is extremely likely)</p>
+										<p class="question-choices" style="font-size: 12px; color: #424242; font-weight: bold;"><input type="radio" /> Not at All Likely (0)<br />
+											<input type="radio" /> (1)<br />
+											<input type="radio" /> (2)<br />
+											<input type="radio" /> (3)<br />
+											<input type="radio" /> (4)<br />
+											<input type="radio" /> (5)<br />
+											<input type="radio" /> (6)<br />
+											<input type="radio" /> (7)<br />
+											<input type="radio" /> (8)<br />
+											<input type="radio" /> (9)<br />
+											<input type="radio" /> Extremely Likely (10)</p>
+										<p class="question-title" style="font-size: 12px; color: #424242; font-weight: bold; margin-top: 50px;">Considering your complete experience with our company, how likely would you be to recommend our company to a friend or colleague? (0 is not at all likely, 10 is extremely likely)</p>
+										<p class="question-choices" style="font-size: 12px; color: #424242; font-weight: bold;"><input type="radio" /> Not at All Likely (0)<br />
+											<input type="radio" /> (1)<br />
+											<input type="radio" /> (2)<br />
+											<input type="radio" /> (3)<br />
+											<input type="radio" /> (4)<br />
+											<input type="radio" /> (5)<br />
+											<input type="radio" /> (6)<br />
+											<input type="radio" /> (7)<br />
+											<input type="radio" /> (8)<br />
+											<input type="radio" /> (9)<br />
+											<input type="radio" /> Extremely Likely (10)</p>
+										<p class="question-title" style="font-size: 12px; color: #424242; font-weight: bold; margin-top: 50px;">Finally, please tell us a little about yourself&#8230;</p>
+										<p class="question-title" style="font-size: 12px; color: #424242; font-weight: bold; margin-top: 10px;">What is your gender?</p>
+										<p class="question-choices" style="font-size: 12px; color: #424242; font-weight: bold;"><input type="radio" /> Male<br />
+											<input type="radio" /> Female</p>
+										<p class="question-title" style="font-size: 12px; color: #424242; font-weight: bold; margin-top: 50px;">How old are you?</p>
+										<p class="question-choices" style="font-size: 12px; color: #424242; font-weight: bold;"><input type="radio" /> Under 13<br />
+											<input type="radio" /> 13-17<br />
+											<input type="radio" /> 18-25<br />
+											<input type="radio" /> 26-34<br />
+											<input type="radio" /> 35-54<br />
+											<input type="radio" /> 55-64<br />
+											<input type="radio" /> 65 or over</p>
+										<p class="question-title" style="font-size: 12px; color: #424242; font-weight: bold; margin-top: 50px;">What is your current marital status?</p>
+										<p class="question-choices" style="font-size: 12px; color: #424242; font-weight: bold;"><input type="radio" /> Single, never married<br />
+											<input type="radio" /> Married without children<br />
+											<input type="radio" /> Married with children<br />
+											<input type="radio" /> Divorced<br />
+											<input type="radio" /> Separated<br />
+											<input type="radio" /> Widowed<br />
+											<input type="radio" /> Living with partner</p>
+										<p class="question-title" style="font-size: 12px; color: #424242; font-weight: bold; margin-top: 50px;">What is the highest level of education you have completed?</p>
+										<p class="question-choices" style="font-size: 12px; color: #424242; font-weight: bold;"><input type="radio" /> Less than High School<br />
+											<input type="radio" /> High School / GED<br />
+											<input type="radio" /> Some College<br />
+											<input type="radio" /> 2-year College Degree<br />
+											<input type="radio" /> 4-year College Degree Masters Degree<br />
+											<input type="radio" /> Doctoral Degree<br />
+											<input type="radio" /> Professional Degree (JD, MD)</p>
+									</div>
+
+								</div>
+							</form>
+						</div>
+						<!-- /.col-lg-3 (nested) -->
+					</div>
+					<!-- /.row (nested) -->
+				</div>
+				<!-- /.panel-body -->
+			</div>
+			<!-- /.panel -->
+		</div>
+		<!-- /.col-lg-12 -->
+	</div>
+
+
+	<?php include "includes/footer.php" ?>
---
+++ userp.php
@@ -0,0 +1,124 @@
+<?php include "includes/header.php" ?>
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-4">
+                <div class="login-panel panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Welcome User</h3>
+                    </div>
+                    <div class="panel-body">
+                    <div class="row">
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-comments fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class="huge">26</div>
+                                    <div>POINTS</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Points</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-green">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-certificate fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class="huge">2</div>
+                                    <div>New SURVEYS!</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Profile</span>
+                                <span class="pull-right"><i class="fa fa-user"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-yellow">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-wrench fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">Edit Profile</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-red">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-user fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">Logout</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!-- /.row -->
+                        <!-- /.panel-heading -->
+                <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <div class="alert alert-success">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. <a href="#" class="alert-link">Alert Link</a>.
+                            </div>
+                            <div class="alert alert-info">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. <a href="#" class="alert-link">Alert Link</a>.
+                            </div>
+                            <div class="alert alert-warning">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. <a href="#" class="alert-link">Alert Link</a>.
+                            </div>
+                            <div class="alert alert-danger">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. <a href="#" class="alert-link">Alert Link</a>.
+                            </div>
+                        </div>
+                        <!-- .panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php include "includes/footer.php" ?>
