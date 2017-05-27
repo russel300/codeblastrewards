a54f6185ca7ad4821868b6673c0fd7a77a38cf2b
hilars <hilkansiime@gmail.com>
2017-05-27 10:06:46 +0300

    Update Database
    Adding new tables,sample data for testing

--- rewards_db.sql
+++ rewards_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 26, 2017 at 10:47 PM
+-- Generation Time: May 27, 2017 at 09:05 AM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -37,7 +37,8 @@ CREATE TABLE `business_partner` (
 --
 
 INSERT INTO `business_partner` (`p_id`, `partner_name`, `partner_desc`) VALUES
-(0, 'none', 'none');
+(0, 'none', 'none'),
+(2, 'Afrocon Limited', 'We deal in Event Management');
 
 -- --------------------------------------------------------
 
@@ -53,6 +54,21 @@ CREATE TABLE `category` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `forms`
+--
+
+CREATE TABLE `forms` (
+  `f_id` int(11) NOT NULL,
+  `input_type` int(11) NOT NULL,
+  `input_name` varchar(80) NOT NULL,
+  `input_id` varchar(80) NOT NULL,
+  `input_value` varchar(80) NOT NULL,
+  `survey_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `survey`
 --
 
@@ -60,6 +76,7 @@ CREATE TABLE `survey` (
   `survey_id` int(11) NOT NULL,
   `survey_name` varchar(80) NOT NULL,
   `survey_url` text NOT NULL,
+  `points` int(11) NOT NULL,
   `p_id` int(11) NOT NULL,
   `cat_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -80,6 +97,13 @@ CREATE TABLE `user` (
   `p_id` int(11) DEFAULT NULL,
   `pwd` varchar(80) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`uid`, `full_names`, `username`, `email`, `phone_number`, `access_level`, `p_id`, `pwd`) VALUES
+(1, 'Administrator', 'admin', 'admin@servrewards.com', '795846321', 'admin', NULL, '21232f297a57a5a743894a0e4a801fc3');
 
 -- --------------------------------------------------------
 
@@ -107,6 +131,13 @@ ALTER TABLE `business_partner`
 --
 ALTER TABLE `category`
   ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indexes for table `forms`
+--
+ALTER TABLE `forms`
+  ADD PRIMARY KEY (`f_id`),
+  ADD KEY `survey_id` (`survey_id`);
 
 --
 -- Indexes for table `survey`
@@ -138,12 +169,17 @@ ALTER TABLE `user_survey`
 -- AUTO_INCREMENT for table `business_partner`
 --
 ALTER TABLE `business_partner`
-  MODIFY `p_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `p_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
   MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `forms`
+--
+ALTER TABLE `forms`
+  MODIFY `f_id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `survey`
 --
@@ -153,17 +189,23 @@ ALTER TABLE `survey`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- Constraints for dumped tables
 --
 
 --
+-- Constraints for table `forms`
+--
+ALTER TABLE `forms`
+  ADD CONSTRAINT `forms_ibfk_1` FOREIGN KEY (`survey_id`) REFERENCES `survey` (`survey_id`);
+
+--
 -- Constraints for table `survey`
 --
 ALTER TABLE `survey`
-  ADD CONSTRAINT `survey_ibfk_2` FOREIGN KEY (`cat_id`) REFERENCES `category` (`cat_id`),
-  ADD CONSTRAINT `survey_ibfk_1` FOREIGN KEY (`p_id`) REFERENCES `business_partner` (`p_id`);
+  ADD CONSTRAINT `survey_ibfk_1` FOREIGN KEY (`p_id`) REFERENCES `business_partner` (`p_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `survey_ibfk_2` FOREIGN KEY (`cat_id`) REFERENCES `category` (`cat_id`);
 
 --
 -- Constraints for table `user`
